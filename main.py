267588524e06847ef5efc574f4289c56609c7092
Sahil Gulati <64752326+sahil150395@users.noreply.github.com>
2021-08-25 02:37:24 +0000

    Test case passed

--- main.py
+++ main.py
@@ -8,10 +8,21 @@ student_scores = {
 # 🚨 Don't change the code above 👆
 
 #TODO-1: Create an empty dictionary called student_grades.
-
+student_grades = {}
 
 #TODO-2: Write your code below to add the grades to student_grades.👇
-
+for name in student_scores:
+    grade = ""
+    if student_scores[name] <= 70:
+        grade = "Fail"
+    elif student_scores[name] <=80:
+        grade = "Acceptable"
+    elif student_scores[name] <=90:
+        grade = "Exceeds Expectations"
+    elif student_scores[name] <=100:
+        grade = "Outstanding"
+    
+    student_grades[name] = grade
     
 
 # 🚨 Don't change the code below 👇
